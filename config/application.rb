efda9fdfcbdbf89d59b0dc3f6d943884a7f880e6
davidrs <david.rs@hotmail.com>
2013-12-23 22:16:46 -0500

    Updated config to allow cross origin requests. Lets it work better as a service.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module Robocoder
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]

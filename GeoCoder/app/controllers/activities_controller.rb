2c9f56dc773069608cca84541a511f8bdce9bf88
davidrs <david.rs@hotmail.com>
2014-01-02 16:50:27 -0500

    Make it more flexible for IATI styles.

--- GeoCoder/app/controllers/activities_controller.rb
+++ GeoCoder/app/controllers/activities_controller.rb
@@ -25,8 +25,10 @@ class ActivitiesController < ApplicationController
 			if activity_obj.nil?
 				activity_obj = Activity.create(identifier: activity["uid"], precision: activity["precision"])
 
-				activity["positions"].each do |position|
-					activity_obj.positions << Position.create(lat: position["latitude"].to_f, long: position["longitude"].to_f, address: position["address"])
+				if not activity["positions"].nil?
+					activity["positions"].each do |position|
+						activity_obj.positions << Position.create(lat: position["latitude"].to_f, long: position["longitude"].to_f, address: position["address"])
+					end
 				end
 
 			end
@@ -44,7 +46,7 @@ class ActivitiesController < ApplicationController
 			rcpt_country = activity.at("recipient-country")
 			
 			# eliminate records with multi country data
-			if rcpt_country and rcpt_country.attr("percentage") == "100"
+			if rcpt_country and (!rcpt_country.attr("percentage") or rcpt_country.attr("percentage") == "100")
 
 				# find country name
 				country = IsoCountryCodes.find(rcpt_country.attr("code")).name
@@ -53,7 +55,7 @@ class ActivitiesController < ApplicationController
 				Nokogiri::XML(activity.to_s).xpath("//description").each do |desc|
 
 					# find the correct and English description 
-					if desc.attr("type") and desc.attr("xml:lang") == "en"
+					if desc.attr("type") and (!desc.attr("xml:lang") or desc.attr("xml:lang") == "en")
 
 						activity_obj = Activity.find_by_identifier iati_id
 
